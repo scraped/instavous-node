02c2280e1104a095c786f4c8bff9a14c56ebbd6d
Nathaniel Ledford <nateledford@fastmail.com>
2018-09-06 13:50:29 -0400

    Add utility methods

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ export const sleep = async (ms: number): Promise<{}> => {
         setTimeout(resolve, ms)
     })
 }
+
+/**
+ * SOURCE: https://gist.github.com/Integralist/749153aa53fea7168e7e#gistcomment-1457123
+ * @param array 
+ */
+export const flattenArray = (array: Array<any>): Array<any> => {
+    const flattenedArray: Array<any> = [].concat(...array)
+    return flattenedArray.some(Array.isArray) 
+        ? flattenArray(flattenedArray) 
+        : flattenedArray
+}
+
+export const range = (start: number = 0, end: number = 1) => {
+    const length = end - start
+    return Array.from({ length }, (_, i) => start + i)
+}
