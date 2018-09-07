7d3a018c8e42bc09322724c411469782e2f5bfae
Nathaniel Ledford <nateledford@fastmail.com>
2018-09-07 08:58:54 -0400

    Remove utility methods

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,20 +4,3 @@ export const sleep = async (ms: number): Promise<{}> => {
         setTimeout(resolve, ms)
     })
 }
-
-/**
- * SOURCE: https://gist.github.com/Integralist/749153aa53fea7168e7e#gistcomment-1457123
- * @param array An array to flatten
- * @returns     A flattened array
- */
-export const flattenArray = <T>(array: T[]): T[] => {
-    const flattenedArray: T[] = ([] as T[]).concat(...array)
-    return flattenedArray.some(Array.isArray)
-        ? flattenArray(flattenedArray)
-        : flattenedArray
-}
-
-export const range = (start: number = 0, end: number = 1) => {
-    const length = end - start
-    return Array.from({ length }, (_, i) => start + i)
-}
