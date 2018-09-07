80580492369236de6a248c9c8a1a66d45e21e2e7
Nathaniel Ledford <nateledford@fastmail.com>
2018-09-07 07:41:06 -0400

    Retrieve app information without directly importing package.json

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,12 +1,10 @@
 
-const pkg = require('../../package.json')
-
 export default class App {
     public static getAppName(): string {
-        return pkg.name
+        return process.env.npm_package_name as string
     }
 
     public static getAppVersion(): string {
-        return pkg.version
+        return process.env.npm_package_version as string
     }
 }
